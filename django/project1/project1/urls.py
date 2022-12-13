4251b378377cc19d867a310edaaba34faeb06634
Suprame4 <adriancortesgonz@gmail.com>
2022-12-12 21:55:48 -0800

    Completed the tasks app

--- django/project1/project1/urls.py
+++ django/project1/project1/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myapp/', include("myapp.urls")),
-    path('newyear/', include("newyear.urls"))
+    path('newyear/', include("newyear.urls")),
+    path('tasks/', include("tasks.urls"))
 ]
